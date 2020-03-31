226f3f62351b71fd0b82d3f64c1985871d17375b
John Mola <johnmmola@gmail.com>
2020-03-31 17:56:09 -0600

    final assignments

--- data/data_wrangling/01a_bibtex_wrangling_assignment.R
+++ data/data_wrangling/01a_bibtex_wrangling_assignment.R
@@ -13,14 +13,27 @@ library(tidyverse)
 library(bibliometrix)
 
 
+# HELPFUL FUNCTION --------------------------------------------------------
+
+named_group_split <- function(.tbl, ...) {
+  grouped <- group_by(.tbl, ...)
+  names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = " / ")))
+  
+  grouped %>% 
+    group_split() %>% 
+    rlang::set_names(names)
+}
+
 # PARAMETERS --------------------------------------------------------------
 
 # define the percent of papers to sample
-percent_to_sample <- 0.10
+percent_to_sample <- 0.25
 
 # the humans who will be reviewing papers
-humans <-  c("John M", "Jon Koch", "Janean S", "Ana M")
+humans <-  c("john_mola", "jon_koch", "janean_sharkey", "ana_montero", "byron_love", "tien_lindsey", "kiera_newman")
 
+# a seed for consistent results
+set.seed(seed = 1912) # the publication year of Sladen's Humble Bee
 
 # DATA SOURCES ------------------------------------------------------------
 
@@ -66,14 +79,15 @@ df_bib_percent_year <- df_bib_duplicate_author_removed %>%
 # ASSIGNMENTS -------------------------------------------------------------
 
 
-assignments_percent_total <- df_bib_percent_total %>% 
-  mutate(assigned_to = rep(sample(humans), length = nrow(.)))
+# assignments_percent_total <- df_bib_percent_total %>% 
+#   mutate(assigned_to = rep(sample(humans), length = nrow(.)))
 
 assignments_percent_year <- df_bib_percent_year %>% 
   mutate(assigned_to = rep(sample(humans), length = nrow(.)))
+  
 
 # check that it worked
-assignments_percent_total %>% group_by(assigned_to) %>% tally()
+# assignments_percent_total %>% group_by(assigned_to) %>% tally()
 assignments_percent_year %>% group_by(assigned_to) %>% tally()
 
 
@@ -83,9 +97,13 @@ write_csv(df_bib_wrangled, "./data/data_output/full_WOK_papers_list_wrangled.csv
 
 write_csv(df_bib_duplicate_author_removed, "./data/data_output/no_dup_author_year_WOK_papers_list_wrangled.csv")
 
-write_csv(assignments_10total, "./data/data_output/assigned_no_dup_10percent_total.csv")
+#write_csv(assignments_10total, "./data/data_output/assigned_no_dup_10percent_total.csv")
 
-write_csv(assignments_10year, "./data/data_output/assigned_no_dup_10percent_year.csv")
+# write_csv(assignments_percent_year, "./data/data_output/assigned_no_dup_25percent_year.csv")
+
+assignments_percent_year %>% 
+  group_by(assigned_to) %>%
+  group_walk(~ write_csv(.x, paste0("./data/data_output/assignments/",.y$assigned_to, "_assigned_noDup_25percent_byYear.csv")))
 
 
 
@@ -106,5 +124,7 @@ combined_plot <- df_combined %>%
   labs(x = "Publication Year", y = "Number of Publications \n Passing Filter", color = "How randomly selected?") +
   scale_color_brewer(type = "qual", palette = 2, labels = c(paste0(percent_to_sample*100, "% Each Year"), paste0(percent_to_sample*100, "% Total"))) +
   theme(legend.position = c(0.3, 0.8), legend.title = element_blank())
+
+combined_plot
 
 ggsave(plot = combined_plot, "./figures/compare_byyear_versus_total_sampling.tiff", dpi = 300, height = 4, width = 5, units = "in")  
